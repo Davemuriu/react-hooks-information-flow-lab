62bbdc1c7c9cca0ad5852e05e1b4ca0339b22293
Davemuriu <dmuriu0@gmail.com>
2025-09-10 23:09:07 +0300

    react hooks information flow lab

---
+++ src/components/Filter.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Filter({ onCategoryChange }) {
+  function handleChange(e) {
+    if (onCategoryChange) {
+      onCategoryChange(e.target.value);
+    }
+  }
+
+  return (
+    <div className="Filter">
+      <select name="filter" onChange={handleChange}>
+        <option value="All">Filter by category</option>
+        <option value="Produce">Produce</option>
+        <option value="Dairy">Dairy</option>
+        <option value="Dessert">Dessert</option>
+      </select>
+    </div>
+  );
+}
+
+export default Filter;
